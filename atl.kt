6206da6df0f33cda7202b3fb19ef703d75f718fb
Geoffrey Owuor <120029033+jeffx-3@users.noreply.github.com>
2023-01-04 19:16:51 +0300

    Update atl.kt

--- atl.kt
+++ atl.kt
@@ -1,8 +1,30 @@
 //This simple code lists the arry
 
-import java.util.*
-fun main(args: Array<String>) {
-    val array = arrayOf("a", "b")
-    val list = Array.toList(*array)
-    println(list)
+fun main() {
+    var name= arrayOf("Larry","Evelyne","Rachael","Tricia")
+    println(name[0])
+        println(name[1])
+    println(name[2])
+    println(name[3])
+    name[0]="peto"
+    println(name[0])
+
+    var age=ArrayList<Int>()
+    age.add(23)
+        age.add(16)
+    age.add(13)
+    age.add(1)
+    println(age[0])
+        println(age[1])
+    println(age[2])
+    println(age[3])
+println(name.first())
+println(name.last())
+println(name.size)
+
+println(age.size)
+age.clear()
+println(age.size)
+
+
 }
